9aae6114f2311b7381112324cc55ca1866e3a8b6
bentrd <benjamin@tordjman.com>
2025-04-29 15:36:27 +0200

    Ajout de highlight pour montrer le lien actif

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 // Composant Navbar
 // Un composant de barre de navigation qui affiche des liens vers différentes pages de l'application.
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
   // Récupération du token d'authentification depuis le stockage local
   const token = localStorage.getItem('token');
+  const location = useLocation();
 
   // Fonction de gestion de la déconnexion
   const handleLogout = () => {
@@ -17,13 +18,13 @@ function Navbar() {
     <nav style={styles.navbar}>
       {!token ? (
         <>
-          <Link style={styles.link} to="/">Connexion</Link>
-          <Link style={styles.link} to="/register">Inscription</Link>
+          <Link style={{...styles.link, ...(location.pathname === "/" ? styles.active : {})}} to="/">Connexion</Link>
+          <Link style={{...styles.link, ...(location.pathname === "/register" ? styles.active : {})}} to="/register">Inscription</Link>
         </>
       ) : (
         <>
-          <Link style={styles.link} to="/profile">Profil</Link>
-          <Link style={styles.link} to="/books">Mes Livres</Link>
+          <Link style={{...styles.link, ...(location.pathname === "/profile" ? styles.active : {})}} to="/profile">Profil</Link>
+          <Link style={{...styles.link, ...(location.pathname === "/books" ? styles.active : {})}} to="/books">Mes Livres</Link>
           <button style={styles.button} onClick={handleLogout}>Déconnexion</button>
         </>
       )}
@@ -58,6 +59,9 @@ const styles = {
     cursor: 'pointer',
     transition: 'background-color 0.3s',
   },
+  active: {
+    textDecoration: 'underline',
+  }
 };
 
 export default Navbar;
